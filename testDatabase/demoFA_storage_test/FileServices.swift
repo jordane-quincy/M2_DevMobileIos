8db9812fdf3ca534885bf4cbc5c1dd8bdec259b1
MAC ISTV <macistv@MacBook-Pro-de-MAC.local>
2017-04-11 13:56:25 +0200

    un debut du fileServices

---
+++ testDatabase/demoFA_storage_test/FileServices.swift
@@ -0,0 +1,18 @@
+//
+//  FileServices.swift
+//  demoFA_storage_test
+//
+//  Created by MAC ISTV on 11/04/2017.
+//  Copyright © 2017 DeptInfo. All rights reserved.
+//
+
+import Foundation
+
+// Class that manage files
+class FileServices {
+    
+    
+    public func createJSONFileFromString(JSONStringify: String) {
+        let data = JSONStringify.data(using: .utf8)!
+    }
+}
